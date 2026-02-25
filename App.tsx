431958176f70631be97e003ba8552190db94efae
tvcorporativafluidsystem <tvcorporativafluidsystem@gmail.com>
2026-02-25 07:55:32 -0300

    Delete App.tsx

--- App.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { RouterProvider } from 'react-router-dom'
-import { router } from './router'
-import './styles/index.css'
-
-export default function App() {
-  return <RouterProvider router={router} />
-}
