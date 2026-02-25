ae276bf8e830bce8786aeabee8703897d4e6e4e6
tvcorporativafluidsystem <tvcorporativafluidsystem@gmail.com>
2026-02-25 07:54:01 -0300

    Delete components directory

--- components/common/Skeleton.tsx
+++
@@ -1,4 +0,0 @@
-import React from 'react'
-export default function Skeleton({ className = '' }: { className?: string }) {
-  return <div className={`animate-pulse bg-slate-800/60 rounded ${className}`} />
-}
--- components/tv/MediaRenderer/ImageViewer.tsx
+++
@@ -1,9 +0,0 @@
-import React, { useEffect } from 'react'
-
-export default function ImageViewer({ src, duration, onDone }: { src: string; duration: number; onDone: () => void }) {
-  useEffect(() => {
-    const t = setTimeout(onDone, (duration || 10) * 1000)
-    return () => clearTimeout(t)
-  }, [src, duration])
-  return <img src={src} className="w-full h-full object-contain" />
-}
